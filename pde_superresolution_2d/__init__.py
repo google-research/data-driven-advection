b3fbe1b65131ab64110d267b3d052087b7cc5981
Googler <noreply@google.com>
2019-06-07 18:36:38 -0700

    Project import generated by Copybara.
    PiperOrigin-RevId: 252156886

--- pde_superresolution_2d/__init__.py
+++ pde_superresolution_2d/__init__.py
@@ -1,5 +1,4 @@
 """2D PDE superresolution models."""
 from pde_superresolution_2d import advection
 from pde_superresolution_2d import core
-from pde_superresolution_2d import floods
 from pde_superresolution_2d import pipelines
